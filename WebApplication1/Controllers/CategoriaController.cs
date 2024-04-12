f5891acb9f846d7050268995b44c9b4ea85ec859
Lucas Gomez <lucasgomez675@gmail.com>
2024-04-12 08:55:17 -0300

    Configuracion de controllers de producto y categoria

---
+++ WebApplication1/Controllers/CategoriaController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly DBProductosWebApi _context;
+
+        public CategoriaController(DBProductosWebApi context)
+        {
+            _context = context;
+        }
+        // GET api/Categoria
+        [HttpPost]
+        public IActionResult Post(Categoria categoria)
+        {
+            _context.Add(categoria);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        // GET api/Categoria
+        [HttpGet]
+        public IEnumerable<Categoria> Get()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        // GET api/Categoria/id
+        [HttpGet("{id}")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+
+        }
+
+        // GET api/Categoria/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int Id, [FromBody] Categoria categoria)
+        {
+            if (Id != categoria.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // GET api/Categoria/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int Id)
+        {
+            var categoria = _context.Categorias.First(x => x.Id == Id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(categoria);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductoController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly DBProductosWebApi _context;
+
+        public ProductoController(DBProductosWebApi context)
+        {
+            _context = context;
+        }
+        // GET api/Producto
+        [HttpPost]
+        public IActionResult Post(Producto producto)
+        {
+            _context.Add(producto);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        // GET api/Producto
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return _context.Productos.ToList();
+        }
+
+        // GET api/Producto/id
+        [HttpGet("{id}")]
+        public ActionResult<Producto> Get(int id)
+        {
+            var producto = _context.Productos.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return producto;
+
+        }
+
+        // GET api/Producto/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int Id, [FromBody] Producto producto)
+        {
+            if (Id != producto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(producto).State = EntityState.Modified;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // GET api/Producto/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int Id)
+        {
+            var producto = _context.Productos.First(x => x.Id == Id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(producto);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
